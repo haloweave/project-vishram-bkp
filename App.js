3a05258757522df7b26ef83cda2dec10c1aa9c38
Abhi <abhi9739@github.com>
2022-11-23 16:17:45 +0530

    Button Added

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {TouchableOpacity, StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Lets start mobile appssss!! Yayyyaa!</Text>
-      <Text>Yeeayyyaa!</Text>
-      <StatusBar style="auto" />
+const Separator = () => (
+  <View style={styles.separator} />
+);
+
+const App = () => (
+  <SafeAreaView style={styles.container}>
+    <View>
+<TouchableOpacity
+        onPress={() => Alert.alert('Round Button pressed')}
+        style={styles.buttonRound}>
+        <Text>I'm a button</Text>
+      </TouchableOpacity>
     </View>
-  );
-}
+
+  </SafeAreaView>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  buttonRound:{
+    width: 100,
+    height: 100,
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 100,
+    justifyContent: 'center',
+    backgroundColor: 'orange',
   },
 });
+
+export default App;
