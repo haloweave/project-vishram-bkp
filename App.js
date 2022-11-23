bfb90791119b5461cf20cb259801ffc1d569f3c9
cyrilsl <cyrilsl@github.com>
2022-11-23 15:04:08 +0530

    Lests start

--- App.js
+++ App.js
@@ -4,10 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Cyril Here, Not anymore</Text>
-      <Text>Many More Changes!</Text>
-      <Text>Many More Changes!</Text>
+      <Text>Lets start mobile appssss!! Yayyyaa!</Text>
 
       <StatusBar style="auto" />
     </View>
