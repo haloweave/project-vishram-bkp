dfb9d1b8a4e7ecb6708e8339eba0ff03adc7f822
cyrilsl <cyrilsl@github.com>
2022-12-01 13:03:17 +0530

    Added stack navigators to app.js

--- App.js
+++ App.js
@@ -1,36 +1,51 @@
 import React from "react";
-import { StyleSheet, SafeAreaView } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
+import {StyleSheet, SafeAreaView, View} from "react-native";
+import {LinearGradient} from "expo-linear-gradient";
 import SkyBlueDashboard from "./skyBlueDashboard";
-import { app } from "./initFirebase";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Stack = createNativeStackNavigator();
 
 const App = () => (
-  <SafeAreaView style={styles.container}>
-    <LinearGradient
-      // Button Linear Gradient
-      colors={["#4691EB", "#ffffff"]}
-      style={styles.background}
-    >
-      <SkyBlueDashboard />
-    </LinearGradient>
-  </SafeAreaView>
+    <SafeAreaView style={
+        styles.container
+    }>
+
+
+
+            <NavigationContainer>
+
+                <Stack.Navigator>
+                <Stack.Screen name="Skyblue" component={SkyBlueDashboard} />
+
+                   
+                </Stack.Navigator>
+              
+            </NavigationContainer>
+        
+    </SafeAreaView>
+
 );
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor: '#FFFFFF' 
+    },
 
-  background: {
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    flex: 1,
-    width: "100%",
-  },
+    background: {
+        position: "absolute",
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        flex: 1,
+        width: "100%",
+       
+    }
 });
 
 export default App;
